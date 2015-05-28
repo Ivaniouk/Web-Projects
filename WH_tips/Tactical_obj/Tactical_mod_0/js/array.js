ff946d8e22826763049bd1187e67d70a086d21fd
Ivaniouk <ivaniouk.e@gmail.com>
2015-05-28 12:57:39 +0300

    arr description

--- WH_tips/Tactical_obj/Tactical_mod_0/js/array.js
+++ WH_tips/Tactical_obj/Tactical_mod_0/js/array.js
@@ -1,187 +1,249 @@
 var Res11 = {
 			Position: 0,
 			Number : 11,
-			Text : "Secure Objective 1 - Capture &amp; Control"
+			Text : "Secure Objective 1 - Capture &amp; Control",
+			Description : "Score 1 Victory Point if you control Objective Marker 1 at the end of your turn."
 		};
 var Res12 = {
 			Position: 1,
 			Number : 12,
-			Text : "Secure Objective 2 - Capture & Control"
+			Text : "Secure Objective 2 - Capture & Control",
+			Description : "Score 1 Victory Point if you control Objective Marker 2 at the end of your turn."
 		};
 var Res13 = {
 			Position: 2,
 			Number : 13,
-			Text : "Secure Objective 3 - Capture & Control"
+			Text : "Secure Objective 3 - Capture & Control",
+			Description : "Score 1 Victory Point if you control Objective Marker 3 at the end of your turn."
 		};
 var Res14 = {
 			Position: 3,
 			Number : 14,
-			Text : "Secure Objective 4 - Capture & Control"
+			Text : "Secure Objective 4 - Capture & Control",
+			Description : "Score 1 Victory Point if you control Objective Marker 4 at the end of your turn."
 		};
 var Res15 = {
 			Position: 4,
 			Number : 15,
-			Text : "Secure Objective 5 - Capture & Control"
+			Text : "Secure Objective 5 - Capture & Control",
+			Description : "Score 1 Victory Point if you control Objective Marker 5 at the end of your turn."
 		};
 var Res16 = {
 			Position: 5,
 			Number : 16,
-			Text : "Secure Objective 6 - Capture & Control"
+			Text : "Secure Objective 6 - Capture & Control",
+			Description : "Score 1 Victory Point if you control Objective Marker 6 at the end of your turn."
 		};
 
 var Res21 = {
 			Position: 6,
 			Number : 21,
-			Text : "Secure Objective 1 - Take & Hold"
+			Text : "Secure Objective 1 - Take & Hold",
+			Description : "Score 1 Victory Point if you control Objective Marker 1 at the end of your turn."
 		};
 var Res22 = {
 			Position: 7,
 			Number : 22,
-			Text : "Secure Objective 2 - Take & Hold"
+			Text : "Secure Objective 2 - Take & Hold",
+			Description : "Score 1 Victory Point if you control Objective Marker 2 at the end of your turn."
 		};
 var Res23 = {
 			Position: 8,
 			Number : 23,
-			Text : "Secure Objective 3 - Take & Hold"
+			Text : "Secure Objective 3 - Take & Hold",
+			Description : "Score 1 Victory Point if you control Objective Marker 3 at the end of your turn."
 		};
 var Res24 = {
 			Position: 9,
 			Number : 24,
-			Text : "Secure Objective 4 - Take & Hold"
+			Text : "Secure Objective 4 - Take & Hold",
+			Description : "Score 1 Victory Point if you control Objective Marker 4 at the end of your turn."
 		};
 var Res25 = {
 			Position: 10,
 			Number : 25,
-			Text : "Secure Objective 5 - Take & Hold"
+			Text : "Secure Objective 5 - Take & Hold",
+			Description : "Score 1 Victory Point if you control Objective Marker 5 at the end of your turn."
 		};
 var Res26 = {
 			Position: 11,
 			Number : 26,
-			Text : "Secure Objective 6 - Take & Hold"
+			Text : "Secure Objective 6 - Take & Hold",
+			Description : "Score 1 Victory Point if you control Objective Marker 6 at the end of your turn."
 		};
 
 var Res31 = {
 			Position: 12,
 			Number : 31,
-			Text : "Secure Objective 1 - Storm & Defend"
+			Text : "Secure Objective 1 - Storm & Defend",
+			Description : "Score 1 Victory Point if you control Objective Marker 1 at the end of your turn."
 		};
 var Res32 = {
 			Position: 13,
 			Number : 32,
-			Text : "Secure Objective 2 - Storm & Defend"
+			Text : "Secure Objective 2 - Storm & Defend",
+			Description : "Score 1 Victory Point if you control Objective Marker 2 at the end of your turn."
 		};
 var Res33 = {
 			Position: 14,
 			Number : 33,
-			Text : "Secure Objective 3 - Storm & Defend"
+			Text : "Secure Objective 3 - Storm & Defend",
+			Description : "Score 1 Victory Point if you control Objective Marker 3 at the end of your turn."
 		};
 var Res34 = {
 			Position: 15,
 			Number : 34,
-			Text : "Secure Objective 4 - Storm & Defend"
+			Text : "Secure Objective 4 - Storm & Defend",
+			Description : "Score 1 Victory Point if you control Objective Marker 4 at the end of your turn."
 		};		
 var Res35 = {
 			Position: 16,
 			Number : 35,
-			Text : "Secure Objective 5 - Storm & Defend"
+			Text : "Secure Objective 5 - Storm & Defend",
+			Description : "Score 1 Victory Point if you control Objective Marker 5 at the end of your turn."
 		};
 var Res36 = {
 			Position: 17,
 			Number : 36,
-			Text : "Secure Objective 6 - Storm & Defend"
+			Text : "Secure Objective 6 - Storm & Defend",
+			Description : "Score 1 Victory Point if you control Objective Marker 6 at the end of your turn."
 		};
 
 var Res41 = {
 			Position: 18,
 			Number : 41,
-			Text : "Recon - Seize Ground"
+			Text : "Recon - Seize Ground",
+			Description : "If the mission has the Mysterious Objectives mission special rule, score 1 Victory point if "
+						  + " every objective on the battlefield has been identified at the end of this, or any previous turn."
 		};
 var Res42 = {
 			Position: 19,
 			Number : 42,
-			Text : "Behind Enemy Lines - Seize Ground"
+			Text : "Behind Enemy Lines - Seize Ground",
+			Description : "Score 1 Victory Point if one of your scoring units is within 12&#34; of your opponent’s table"
+						  + " edge at the end of your turn. If 3 or more of your scoring units are within 12&#34; of your"
+						  + "opponent’s table edge at the end of your turn, score D3 Victory Points instead."
 		};
 var Res43 = {
 			Position: 20,
 			Number : 43,
-			Text : "Hold The Line - Seize Ground"
+			Text : "Hold The Line - Seize Ground",
+			Description : " Score 1 Victory Point if at least 3 of your scoring units and none of your opponent’s"
+						  + " scoring units are within 12&#34; of your own table edge at the end of your turn."
 		};
 var Res44 = {
 			Position: 21,
 			Number : 44,
-			Text : "Ascendency - Seize Ground"
+			Text : "Ascendency - Seize Ground",
+			Description : "Score D3 Victory Points if you control any three Objective Markers at the end of your turn."
 		};		
 var Res45 = {
 			Position: 22,
 			Number : 45,
-			Text : "Supremacy - Seize Ground"
+			Text : "Supremacy - Seize Ground",
+			Description : "Score D3 Victory Points if you control at least two Objective Markers and at least twice as"
+						  + " many Objective Markers as your opponent controls at the end of your turn."
 		};	
 var Res46 = {
 			Position: 23,
 			Number : 46,
-			Text : "Domination - Seize Ground"
+			Text : "Domination - Seize Ground",
+			Description : "Score D3+3 Victory Points if you control every Objective Marker on the table at the end of your turn."
 		};			
 		
 var Res51 = {
 			Position: 24,
 			Number : 51,
-			Text : "Overwhelming Firepower - Purge"
+			Text : "Overwhelming Firepower - Purge",
+			Description : "Score 1 Victory Point if an enemy unit was completely destroyed during the Shooting"
+						  + " phase of your turn. If you completely destroyed 3 or more enemy units during the"
+						  + " Shooting phase of your turn, score D3 Victory Points instead."
 		};
 var Res52 = {
 			Position: 25,
 			Number : 52,
-			Text : "Blood and Guts - Purge"
+			Text : "Blood and Guts - Purge",
+			Description : "Score 1 Victory Point if an enemy unit was completely destroyed during the Assault phase"
+						  + " of your turn. If you completely destroyed 3 or more enemy units during the Assault phase"
+						  + " of your turn, score D3 Victory Points instead."
 		};
 var Res53 = {
 			Position: 26,
 			Number : 53,
-			Text : "No Prisoners - Purge"
+			Text : "No Prisoners - Purge",
+			Description : "Score 1 Victory Point if an enemy unit was completely destroyed during your turn. If you"
+						  + " completely destroyed between 3 and 5 enemy units during your turn, score D3 Victory"
+						  + " Points instead. If you completely destroyed 6 or more enemy units during your turn,"
+						  + " score D3+3 Victory Points instead."
 		};		
 var Res54 = {
 			Position: 27,
 			Number : 54,
-			Text : "Hungry for Glory - Purge"
+			Text : "Hungry for Glory - Purge",
+			Description : "Score 1 Victory Point if you issued a challenge during your turn. If you issued 3 or more"
+						  + " challenges during your turn, score D3 Victory Points instead."
 		};	
 var Res55 = {
 			Position: 28,
 			Number : 55,
-			Text : "Psychological Warfare - Purge"
+			Text : "Psychological Warfare - Purge",
+			Description : "Score 1 Victory Point if your opponent failed a Morale, Pinning or Fear test during this"
+						  + " turn. If your opponent failed 3 or more Morale, Pinning or Fear tests, score D3 Victory"
+						  + " Points instead."
 		};	
 var Res56 = {
 			Position: 29,
 			Number : 56,
-			Text : "Harness the Warp - Purge"
+			Text : "Harness the Warp - Purge",
+			Description : " Score 1 Victory Point if you successfully manifested a Psychic Power during your turn. If"
+						  + " you successfully manifested 3 or more Psychic Powers, score D3 Victory Points instead."
 		};
 	
 var Res61 = {
 			Position: 30,
 			Number : 61,
-			Text : "Kingslayer - Annihilation"
+			Text : "Kingslayer - Annihilation",
+			Description : "Score D3 Victory Points at the end of your turn if your opponent’s Warlord has been"
+						  + " removed as a casualty during this, or any previous turn."
 		};	
 var Res62 = {
 			Position: 31,
 			Number : 62,
-			Text : "Witch Hunter - Annihilation"
+			Text : "Witch Hunter - Annihilation",
+			Description : "Score 1 Victory Point at the end of your turn if at least one enemy unit with the Psyker,"
+						  + " Psychic Pilot or Brotherhood of Psykers/Sorcerers special rule was completely destroyed"
+						  + " during your turn."
 		};	
 var Res63 = {
 			Position: 32,
 			Number : 63,
-			Text : "Scour the Skies - Annihilation"
+			Text : "Scour the Skies - Annihilation",
+			Description : "Score 1 Victory Point at the end of your turn if at least one enemy Flyer or Flying"
+						  + " Monstrous Creature was removed as a casualty during your turn."
 		};
 var Res64 = {
 			Position: 33,
 			Number : 64,
-			Text : "Assassinate - Annihilation"
+			Text : "Assassinate - Annihilation",
+			Description : "Score 1 Victory Point at the end of your turn if at least one enemy character was removed"
+						  + " as a casualty during your turn. If 3 or more enemy characters were removed as casualties"
+						  + " during your turn, score D3 Victory Points instead."
 		};	
 var Res65 = {
 			Position: 34,
 			Number : 65,
-			Text : "Demolitions - Annihilation"
+			Text : "Demolitions - Annihilation",
+			Description : "Score 1 Victory Point at the end of your turn if at least one gun emplacement or enemy"
+						  + " building was destroyed during your turn. If at least one enemy building with the Mighty"
+						  + " Bulwark special rule was destroyed during your turn, score D3 Victory Points instead."
 		};		
 var Res66 = {
 			Position: 35,
 			Number : 66,
-			Text : "Big Game Hunter - Annihilation"
+			Text : "Big Game Hunter - Annihilation",
+			Description : "Score 1 Victory Point at the end of your turn if at least one enemy Vehicle or Monstrous"
+						  + " Creature was destroyed during your turn. If at least one enemy Super-heavy vehicle or"
+						  + " Gargantuan Creature was destroyed during your turn, score D3 Victory Points instead."
 		};	
 		
 		
