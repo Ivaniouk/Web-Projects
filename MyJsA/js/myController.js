2a9ad7ce225e9f03c4a2f7d8b1713e9eb2a9adb3
Ivaniouk <ivaniouk.e@gmail.com>
2015-09-15 00:04:22 +0300

    Added global variable (service)

--- MyJsA/js/myController.js
+++ MyJsA/js/myController.js
@@ -1,25 +1,90 @@
-function RuleListCtrl($scope){
-	$scope.rules = [
+//Global variable
+var myApp = angular.module('myApp',[]);
+//service
+myApp.factory('UserService', function() {
+	return {
+		name : 'One'
+	};
+});
+
+
+
+
+function RuleListCtrl($scope, UserService){
+	//$scope.rules = Complicated;
+	alert(UserService.name);
+	switch(UserService.name){
+		case 'One':
+			$scope.rules = simple;
+			break;
+		case 'Two':
+			$scope.rules = Complicated;
+			break;
+		case 'Three':
+			$scope.rules = difficult;
+			break;
+		case 'Four':
+			$scope.rules = difficult;
+			break;
+	}
+}
+
+
+function MainMenuCtrl($scope, UserService){
+	$scope.btnMainArr = ['One', 'Two', 'Three', 'Four'];
+	
+	$scope.showBtnName = function(btnName){
+		UserService.name = btnName;
+		
+	}
+}
+
+
+
+var simple = [
 	{"name": "One Simple",
 	 "description": "This is a first Simple rule"},
 	{"name": "Two Simple",
 	 "description": "This is a second Simple rule"}, 
 	{"name": "Three Simple",
-	 "description": "This is a third Simple rule"},
-	 {"name": "Four Simple",
-	 "description": "This is a fourth Simple rule"},
-	 {"name": "Five Simple",
-	 "description": "This is a fifth Simple rule"},
-	 {"name": "Six Simple",
-	 "description": "This is a sixth Simple rule"}
+	"description": "This is a third Simple rule"},
+	{"name": "Four Simple",
+	"description": "This is a fourth Simple rule"},
+	{"name": "Five Simple",
+	"description": "This is a fifth Simple rule"},
+	{"name": "Six Simple",
+	"description": "This is a sixth Simple rule"}
 	];
-}
-
-
-function MainMenuCtrl($scope){
-	$scope.btnMainArr = ['One', 'Two', 'Three', 'Four'];
 	
-	$scope.showBtnName = function(btnName){
-		alert(btnName);
-	}
-}
+var Complicated = [
+	{"name": "One Complicated",
+	 "description": "This is a first Complicated rule"},
+	{"name": "Two Complicated",
+	 "description": "This is a second Simple rule"}, 
+	{"name": "Three Complicated",
+	"description": "This is a third Complicated rule"},
+	{"name": "Four Complicated",
+	"description": "This is a fourth Complicated rule"},
+	{"name": "Five Complicated",
+	"description": "This is a fifth Complicated rule"},
+	{"name": "Six Complicated",
+	"description": "This is a sixth Complicated rule"}
+	];
+	
+var difficult = [
+	{"name": "One difficult",
+	 "description": "This is a first difficult rule"},
+	{"name": "Two difficult",
+	 "description": "This is a second Simple rule"}, 
+	{"name": "Three difficult",
+	"description": "This is a third difficult rule"},
+	{"name": "Four difficult",
+	"description": "This is a fourth difficult rule"},
+	{"name": "Five difficult",
+	"description": "This is a fifth difficult rule"},
+	{"name": "Six difficult",
+	"description": "This is a sixth difficult rule"}
+	];
+
+	
+	
