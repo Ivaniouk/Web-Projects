8ccdac0de97468217a22270a8b0ccc51bf2fd7e5
Ivaniouk Alexander <ivaniouk.e@gmail.com>
2015-09-14 17:05:05 +0300

    New Angular

---
+++ MyJsA/js/myController.js
@@ -0,0 +1,16 @@
+function RuleListCtrl($scope){
+	$scope.rules = [
+	{"name": "One Simple",
+	 "description": "This is a first Simple rule"},
+	{"name": "Two Simple",
+	 "description": "This is a second Simple rule"}, 
+	{"name": "Three Simple",
+	 "description": "This is a third Simple rule"},
+	 {"name": "Four Simple",
+	 "description": "This is a fourth Simple rule"},
+	 {"name": "Five Simple",
+	 "description": "This is a fifth Simple rule"},
+	 {"name": "Six Simple",
+	 "description": "This is a sixth Simple rule"}
+	];
+}
