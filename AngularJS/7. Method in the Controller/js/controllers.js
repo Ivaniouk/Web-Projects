307e875d8192b4d228c80770527f8b7728b97c95
Ivaniouk Alexander <ivaniouk.e@gmail.com>
2015-09-14 17:22:24 +0300

    Figured out how to do onclick in Angular

--- AngularJS/7. Method in the Controller/js/controllers.js
+++ AngularJS/7. Method in the Controller/js/controllers.js
@@ -3,6 +3,13 @@ myApp.factory('Data', function(){
 	return {message:"I am data from a service"}
 })
 
+function MainCtrl($scope){
+	$scope.alertFunc = function(){
+		alert("CLICK");
+	}
+	
+}
+
 function FirstCtrl($scope, Data){
 	$scope.data = Data;
 }
