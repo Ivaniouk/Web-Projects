b7238fc34b31046edd251f819657619e2e9bd240
Ivaniouk Alexander <ivaniouk.e@gmail.com>
2015-11-05 18:16:09 +0200

    added background functional

--- Generator/js/MyScript/myFunctions.js
+++ Generator/js/MyScript/myFunctions.js
@@ -1,9 +1,29 @@
+//Background functions
 function ShowBuildBackgroundSelect(){
     var backgroundVar =  $('[name="background"]');
     backgroundVar.removeAttr("disabled");
     backgroundVar.children().first().text("Select Background");
+    //BuildBackgroundAptitudes(backgroundAptitudeVar);
 }
 
+function ShowBackgroundAptitude(){
+    var backgroundAptitudeVar = $('[name="BackgroundAptitude"]');
+    backgroundAptitudeVar.removeAttr("disabled");
+}
+
+function BuildBackgroundAptitudes(val){
+    //find way to transfer names into jQuery
+    OptionBuilder(val);
+}
+
+function OptionBuilder(val){
+    for(var i = 0; i < BackgroundArr[val].Aptitude.length; i++){
+        var option = document.createElement('option');
+        option.innerHTML =  "<option>" + BackgroundArr[val].Aptitude[i] + "</option>";
+        $('[name="BackgroundAptitude"]').append(option);
+    }
+}
+//role functions
 function ShowBuildRoleSelect(){
     var roleVar =  $('[name="role"]');
     roleVar.removeAttr("disabled");
--- Generator/js/MyScript/myScript.js
+++ Generator/js/MyScript/myScript.js
@@ -5,8 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     $('[name="background"]').on('change', function(){
+        var val = $('[name="background"]').find(":selected").val();
+        $('[name="BackgroundAptitude"]').find(":enabled").remove(); //remove previous options
+        BuildBackgroundAptitudes(val);
+        ShowBackgroundAptitude();
+    });
+
+    $('[name="BackgroundAptitude"]').on('change', function(){
         ShowBuildRoleSelect();
     });
+
 
     $('[name="role"]').on('change', function(){
         //getting info from fields and building tables
