e46390180d4a2c4e6aa8ecfef80937ed554a6597
Ivaniouk <ivaniouk.e@gmail.com>
2015-11-09 22:12:49 +0200

    added female names

--- Generator/js/DB/General.js
+++ Generator/js/DB/General.js
@@ -1,6 +1,6 @@
 var arrStat = ["BS", "WS", "Strength", "Toughness", "Agility", "Intelligence", "Perception", "Willpower", "Fellowship"];
 
-var maleNameArr = ["Arz", "Aaz", "Azararch", "Alaric", "Able", "Barik", "Canto", "Baranoth", "Barbosa", "Bones",
+var maleNamesArr = ["Arz", "Aaz", "Azararch", "Alaric", "Able", "Barik", "Canto", "Baranoth", "Barbosa", "Bones",
                     "Carm", "Darrial", "Castess", "Caradoc", "Cutter", "Farn", "Enoch", "Davrus", "Draco", "Dakka",
                     "Garak", "Festus", "Erioch", "Eizen", "Dokk", "Horst", "Gaius", "Galliach", "Ferris", "Ergs",
                     "Imk", "Harlon", "Harthos", "Grendal", "Fanz", "Jart", "Irlven", "Icharus", "Harlocke", "Flair",
@@ -9,9 +9,18 @@ var maleNameArr = ["Arz", "Aaz", "Azararch", "Alaric", "Able", "Barik", "Canto",
                     "Oort", "Nicodemus", "Octavius", "Naveen", "Lax", "Pak", "Quinten", "Partheos", "Oscarl", "Meng",
                     "Ragaa", "Rhaban", "Quellus", "Perrin", "Nast", "Salc", "Silvanto", "Ramalies", "Romulus", "Reddin",
                     "Tarl" "Titus" "Severan" "Sabanth" "Scab"  "Volc" "Uriah" "Tyruss" "Urial" "Shiv", "Yarl", "Xerxes",
-                    "Ventium", "Wolfe", "Tranq",  "Zel", "Zariel", "Zarath", "Zarkov", "Wurm"]
+                    "Ventium", "Wolfe", "Tranq",  "Zel", "Zariel", "Zarath", "Zarkov", "Wurm"];
 
-
+var femaleNamesArr = ["Arda", "Acadi", "Araleen", "Aennisse", "Alta", "Cil", "Balida", "Castella", "Bianca", "Blue",
+                    "Dara", "Cassa", "Elleantra", "Drusilla", "Echo", "Elphet", "Darial", "Fausta", "Eos", "Fatal",
+                    "Frakka", "Frennine", "Flavia", "Florenza", "Flame", "Garma", "Galatia", "Heras", "Genevieve", "Gamma",
+                    "Halli", "Hannette", "Irissa", "Hannette", "Grace", "Ing", "Jinzia", "Jucinda", "Jocasta", "Jaine",
+                    "Julla", "Karroleen", "Lucindia", "Katarina", "Lho", "Kelle", "Liri", "Mycandra", "Lethe", "Mini",
+                    "Merra", "Magdala", "Novianna", "Miranda", "Nerva", "Nimm", "Narcia", "Nuella", "Pollonia", "Newt",
+                    "Nulla", "Pennette", "Palanza", "Rosalind", "Pris", "Paz", "Quineel", "Praenta", "Severine", "Red",
+                    "Russa", "Rhia", "Regia", "Tessera", "Steel", "Sulle", "Sesselie", "Scythia", "Tzarelle", "Starr",
+                    "Thima", "Thesse", "Temetria", "Urania", "Tanda", "Vas","Uri", "Thallia", "Verity", "Trix",
+                    "Yanne", "Xandra", "Venria", "Wynnif", "Xina", "Zanna", "Zadori", "Yemmise", "Yasta", "Zedda"];
 
 
 
