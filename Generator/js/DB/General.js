3b909faa3097cf4dfadae2f8ee36ba7538a88a47
Ivaniouk <ivaniouk.e@gmail.com>
2015-11-09 22:01:59 +0200

    added male names

--- Generator/js/DB/General.js
+++ Generator/js/DB/General.js
@@ -1,1 +1,29 @@
 var arrStat = ["BS", "WS", "Strength", "Toughness", "Agility", "Intelligence", "Perception", "Willpower", "Fellowship"];
+
+var maleNameArr = ["Arz", "Aaz", "Azararch", "Alaric", "Able", "Barik", "Canto", "Baranoth", "Barbosa", "Bones",
+                    "Carm", "Darrial", "Castess", "Caradoc", "Cutter", "Farn", "Enoch", "Davrus", "Draco", "Dakka",
+                    "Garak", "Festus", "Erioch", "Eizen", "Dokk", "Horst", "Gaius", "Galliach", "Ferris", "Ergs",
+                    "Imk", "Harlon", "Harthos", "Grendal", "Fanz", "Jart", "Irlven", "Icharus", "Harlocke", "Flair",
+                    "Kulem", "Jaspar", "Karpath", "Jenquin", "Garrit", "Larl", "Killian", "Lothos", "Karlos", "Hons",
+                    "Mok", "Lazarus", "Marius", "Lemant", "Ills", "Narl", "Mordicai", "Noctine", "Magnus", "Jaq",
+                    "Oort", "Nicodemus", "Octavius", "Naveen", "Lax", "Pak", "Quinten", "Partheos", "Oscarl", "Meng",
+                    "Ragaa", "Rhaban", "Quellus", "Perrin", "Nast", "Salc", "Silvanto", "Ramalies", "Romulus", "Reddin",
+                    "Tarl" "Titus" "Severan" "Sabanth" "Scab"  "Volc" "Uriah" "Tyruss" "Urial" "Shiv", "Yarl", "Xerxes",
+                    "Ventium", "Wolfe", "Tranq",  "Zel", "Zariel", "Zarath", "Zarkov", "Wurm"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
