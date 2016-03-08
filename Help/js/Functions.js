ed470481cea5ab4afb86e0e05eca6fc3fc912ca3
Ivaniouk <ivaniouk.e@gmail.com>
2016-03-08 14:28:57 +0200

    new help

---
+++ Help/js/Functions.js
@@ -0,0 +1,215 @@
+'use strict';
+/**
+	JQuery MODE
+*/
+/*
+//білдимо та апендимо поля доя Work
+function CreateWork() {
+	//створюємо форм груп
+	var $customFormGroup = $('<div></div>');
+	$customFormGroup.attr("class", "form-group");
+	//створюємо лейбл
+	var $labelOne = $('<label></label>');
+	$labelOne.attr("class", "col-sm-2 control-label");
+	$labelOne.html("<?= __('work.place') . '&nbsp;ru' ?>");//вставляємо через innerHTML ПХПешну бяку
+	//створюємо обгортку навколо інпута (div)
+	var $inputWrapOne = $('<div></div>');
+	$inputWrapOne.attr("class", "col-sm-3");
+	$inputWrapOne.append('<input type="text" date-name="_work_place[?][ru]" name="_work_place[?][ru]" />'); //варіант без innerHTML (кажуть шо так тру)
+	//створюємо другий лейбл
+	var $labelTwo = $('<label></label>');
+	$labelTwo.attr("class", "col-sm-2 control-label");
+	$labelTwo.html("<?= __('expirience.work.place') . '&nbsp;ru' ?>");//вставляємо через innerHTML ПХПешну бяку
+	//створюємо обгортку навколо другого інпута (div)
+	var $inputWrapTwo = $('<div></div>');
+	$inputWrapTwo.attr("class", "col-sm-3");
+	$inputWrapTwo.append('<input type="text" date-name="_work_expirience[?][ru]" name="_work_expirience[?][ru]" />'); //варіант без innerHTML (кажуть шо так тру)
+	
+	//додаємо до материнського елемента все шо ми настворювали
+	$customFormGroup.append($labelOne);
+	$customFormGroup.append($inputWrapOne);
+	$customFormGroup.append($labelTwo);
+	$customFormGroup.append($inputWrapTwo);
+	console.log($customFormGroup);
+	//ідемо в DOM і приєднуємо наш новий елемент
+	$(".add_work_form").append($customFormGroup);
+}*/
+/*
+//збирає всі значення з полів у add_work_form
+function GetValuesWork() {
+	var inputValues = [];
+	$(".add_work_form :input").each(function() {
+		if($(this).val() != ""){
+			console.log($(this).val());
+			inputValues.push($(this).val());
+		}
+	});
+	//відправляє пост
+	//$.post("URL тут", JSON.stringify(inputValues));
+}*/
+
+
+
+/**
+	JS MODE
+*/
+/*Get all input fields inside div*/
+//document.getElementById('mydiv').getElementsByTagName('input')
+/*
+var elem = document.getElementsByClassName("add_work_form");
+elem[0].getElementsByTagName('input')[0].value
+*/
+
+
+//білдимо та апендимо поля доя Work
+function CreateWork() {
+	var customFormGroupOne = document.createElement("div");
+	customFormGroupOne.setAttribute("class", "form-group");
+	
+	var labelOne = document.createElement("label");
+	labelOne.setAttribute("class", "col-sm-2 control-label");
+	labelOne.innerHTML = "<?= __('work.place') . '&nbsp;ru' ?>";
+	
+	var inputWrapOne = document.createElement("div");
+	inputWrapOne.setAttribute("class", "col-sm-3");
+	inputWrapOne.innerHTML = '<input type="text" date-name="_work_place[?][ru]" name="_work_place[?][ru]" />';
+	
+	customFormGroupOne.appendChild(labelOne);
+	customFormGroupOne.appendChild(inputWrapOne);
+	
+	var customFormGroupTwo = document.createElement("div");
+	customFormGroupTwo.setAttribute("class", "form-group");
+	
+	var labelTwo = document.createElement("label");
+	labelTwo.setAttribute("class", "col-sm-2 control-label");
+	labelTwo.innerHTML = "<?= __('expirience.work.place') . '&nbsp;ru' ?>";
+	
+	var inputWrapTwo = document.createElement("div");
+	inputWrapTwo.setAttribute("class", "col-sm-3");
+	inputWrapTwo.innerHTML = '<input type="text" date-name="_work_expirience[?][ru]" name="_work_expirience[?][ru]" />';
+	
+	customFormGroupTwo.appendChild(labelTwo);
+	customFormGroupTwo.appendChild(inputWrapTwo);
+	
+	var elem = document.getElementsByClassName("add_work_form");
+	 
+	elem[0].appendChild(customFormGroupOne);
+	elem[0].appendChild(customFormGroupTwo);
+}
+
+
+//збирає всі значення з полів у add_work_form
+function GetValuesWork() {
+	var inputValues = [];
+	var elem = document.getElementsByClassName("add_work_form");
+	var inputArr = elem[0].getElementsByTagName('input');
+		
+	for(var i = 0; i < inputArr.length; i++) {
+		inputValues.push(inputArr[i].value);
+	}
+	
+	console.log(inputValues);
+}
+
+/***********************************PROMISE*********************************************/
+/** 
+	WORKING PROMISE (ON CLICK)
+*/
+/*
+function Done(result) {
+	console.log(result); // "Worked!"
+}
+
+function NotDone(err) {
+	console.log(err.message); // Error: "It broke"
+}
+
+function PromiseWrapper() {
+	var promise = new Promise(function(resolve, reject) {
+		var x = 0;
+		setTimeout(function() {
+			//resolve("result");
+			//reject(new Error("время вышло!"));
+			x = 1;
+			if (x == 1) {
+				resolve("result");
+			} else {
+				reject(new Error("время вышло!"));
+			}
+		}, 3000);
+	});
+	
+	promise.then(Done, NotDone);
+}
+*/
+
+/** RETURN NEW PROMISE AND WRAPPER************************************************/
+
+function Done(result) {
+	console.log(result); // "Worked!"
+}
+
+function NotDone(err) {
+	console.log(err.message); // Error: "It broke"
+}
+
+function ReturnPromise() {
+	return new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			resolve("result");
+		}, 3000);
+	});
+}
+
+//We can use wrapper (or not)
+function ReturnPromiseWrapper() {
+	ReturnPromise().then(Done, NotDone);
+}
+
+
+
+/** TESTING LANG OBJECT************************************************/
+
+var langObjRU = {
+	"Add work place" : "Добавить место работы",
+	"Get work value" : "Получить значение полей",
+	"Promise test" : "Тест Промиса",
+	"Promise returning test" : "Возврат промиса"
+};
+
+var langObjENG = {
+	"Add work place" : "Add work place",
+	"Get work value" : "Get work value",
+	"Promise test" : "Promise test",
+	"Promise returning test" : "Promise returning test"
+};
+
+function GetCurrentLangObj (lang) {
+	
+	switch(lang) {
+		case "en":
+			return langObjENG;
+		break;
+		case "ru":
+			return langObjRU;
+		break;
+		default:
+			return langObjENG; 
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Help/js/Script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () { // канонічний JS варіант навішування лисенера на сторінку 
+	
+	var langObj = GetCurrentLangObj(document.documentElement.lang);
+	
+	
+	//я так зрозумів шо тобі треба в квері стайлі це написать того тут починається квері
+	function appendWork() {
+		CreateWork();
+		CreateWork();
+	}
+	//декларативно вішаємо на кнопку з класом jq_add_new_work онклік івент - експресія зло
+	//$(".jq_add_new_work").on('click', appendWork);
+	var AddWork_BTN = document.getElementsByClassName("jq_add_new_work");
+	AddWork_BTN[0].addEventListener("click", appendWork, false);
+	//декларативно вішаємо на кнопку з класом jq_get_work_value онклік івент
+	//$(".jq_get_work_value").on('click', GetValuesWork);
+	var GetWorkValue_BTN = document.getElementsByClassName("jq_get_work_value");
+	GetWorkValue_BTN[0].addEventListener("click", GetValuesWork, false);
+	
+	//Simple promise ON CLICK
+	//var jq_promise_BTN = document.getElementsByClassName("jq_promise");
+	//jq_promise_BTN[0].addEventListener("click", PromiseWrapper, false);
+	
+	//Return promise ON CLICK
+	var jq_promise_return_BTN = document.getElementsByClassName("jq_promise_retrn");
+	jq_promise_return_BTN[0].addEventListener("click", ReturnPromiseWrapper, false);
+	//jq_promise_return_BTN[0].addEventListener("click", ReturnPromise().then(Done, NotDone), false);
+	
+	
+});
